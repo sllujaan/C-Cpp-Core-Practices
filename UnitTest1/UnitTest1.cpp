366af79eeac1ed54da2010c446f75ceb9d72d697
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-08 13:06:28 +0500

    wrote tests to check cached memory locations

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -26,7 +26,7 @@ namespace UnitTest1
 		}
 
 
-		TEST_METHOD(TestMethod2)
+		TEST_METHOD(Test_cachedMemoryLocations)
 		{
 			LPCWSTR NAMES[] = { L"abc.txt", L"abc1.txt",  L"abc2.txt",
 				L"abc3.txt",  L"abc4.txt",  L"abc5.txt",  L"abc6.txt",
@@ -46,21 +46,21 @@ namespace UnitTest1
 			}
 			_fileTree.initTreeCach();
 			
-			std::vector<MY_FILES::FILE_TREE_STRUCT*>* cachedItems = _fileTree.getTreeIitemsByLevel(5);
-			if (cachedItems == nullptr) return;
-
+			std::vector<MY_FILES::FILE_TREE_STRUCT*>* cachedItems = _fileTree.getTreeIitemsByLevel(1);
+			if (cachedItems == nullptr) {
+				Assert::IsTrue(false);
+			}
 
 			Assert::IsTrue(_fileTree[0] == cachedItems[0][0]);
-			Assert::IsTrue((_fileTree[1] == cachedItems[0][0]));
-			Assert::IsTrue(_fileTree[2] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[3] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[4] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[5] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[6] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[7] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[8] == cachedItems[0][0]);
-			Assert::IsTrue(_fileTree[8] == cachedItems[0][0]);
-
+			Assert::IsTrue(_fileTree[1] == cachedItems[0][1]);
+			Assert::IsTrue(_fileTree[2] == cachedItems[0][2]);
+			Assert::IsTrue(_fileTree[3] == cachedItems[0][3]);
+			Assert::IsTrue(_fileTree[4] == cachedItems[0][4]);
+			Assert::IsTrue(_fileTree[5] == cachedItems[0][5]);
+			Assert::IsTrue(_fileTree[6] == cachedItems[0][6]);
+			Assert::IsTrue(_fileTree[7] == cachedItems[0][7]);
+			Assert::IsTrue(_fileTree[8] == cachedItems[0][8]);
+			Assert::IsTrue(_fileTree[9] == cachedItems[0][9]);
 		}
 
 	};
